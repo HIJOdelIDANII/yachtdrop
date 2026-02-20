62d251bb27159e7d7c346167fc589219079b753f
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2026-02-20 02:36:24 +0100

    fix: add IF EXISTS to drop index migration for prod safety

--- web/prisma/migrations/20260219222743/migration.sql
+++ web/prisma/migrations/20260219222743/migration.sql
@@ -1,35 +1,35 @@
 -- DropIndex
-DROP INDEX "marinas_lat_lng_idx";
+DROP INDEX IF EXISTS "marinas_lat_lng_idx";
 
 -- DropIndex
-DROP INDEX "marinas_name_trgm_idx";
+DROP INDEX IF EXISTS "marinas_name_trgm_idx";
 
 -- DropIndex
-DROP INDEX "idx_order_events_order_id";
+DROP INDEX IF EXISTS "idx_order_events_order_id";
 
 -- DropIndex
-DROP INDEX "idx_order_items_order_id";
+DROP INDEX IF EXISTS "idx_order_items_order_id";
 
 -- DropIndex
-DROP INDEX "idx_orders_created_at";
+DROP INDEX IF EXISTS "idx_orders_created_at";
 
 -- DropIndex
-DROP INDEX "idx_products_available_category";
+DROP INDEX IF EXISTS "idx_products_available_category";
 
 -- DropIndex
-DROP INDEX "idx_products_brand";
+DROP INDEX IF EXISTS "idx_products_brand";
 
 -- DropIndex
-DROP INDEX "idx_products_category_id";
+DROP INDEX IF EXISTS "idx_products_category_id";
 
 -- DropIndex
-DROP INDEX "idx_products_name_trgm";
+DROP INDEX IF EXISTS "idx_products_name_trgm";
 
 -- DropIndex
-DROP INDEX "idx_products_offers";
+DROP INDEX IF EXISTS "idx_products_offers";
 
 -- DropIndex
-DROP INDEX "idx_products_search_vector";
+DROP INDEX IF EXISTS "idx_products_search_vector";
 
 -- DropIndex
-DROP INDEX "idx_products_trending";
+DROP INDEX IF EXISTS "idx_products_trending";
